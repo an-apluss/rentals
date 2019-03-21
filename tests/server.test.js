83fbc380f605c13199d6b70e10fd30614137995c
an-apluss <anuoluwapoakinseye@gmail.com>
2019-03-21 09:06:04 +0100

    Include test to fetch all genres

--- tests/server.test.js
+++ tests/server.test.js
@@ -17,4 +17,18 @@ describe('GET /', () => {
       });
     done();
   });
+
+  describe('Genres', () => {
+    it('get all available genres when this endpoint is hit', (done) => {
+      chai.
+        request(server)
+        .get('api/v1/genres')
+        .end((err, res) => {
+          res.body.should.have.property('status').eql('200');
+          res.body.shoud.be.an('object');
+          res.body.should.have.property('data').eql('object');
+        });
+        done();
+    });
+  });
 });
