4edcca2bc26451800c12f53f4cce67cd7a4d72b3
an-apluss <anuoluwapoakinseye@gmail.com>
2019-03-21 11:24:44 +0100

    Amend test suite

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,30 +5,30 @@ import app from '../server';
 
 chai.use(chaiHttp);
 chai.should();
-
-describe('GET /', () => {
-  it('When / url pattern is encounter return its content', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        res.body.should.have.property('status').eql(200);
-        res.body.should.have.property('message').eql('Hello this is a Rental App');
-        res.body.should.be.an('object');
-      });
-    done();
+describe('Rental', () => {
+  describe('GET /', () => {
+    it('When / url pattern is encounter return its content', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.body.should.have.property('status').eql(200);
+          res.body.should.have.property('message').eql('Hello this is a Rental App');
+          res.body.should.be.an('object');
+        });
+      done();
+    });
   });
-
   describe('Genres', () => {
     it('get all available genres when this endpoint is hit', (done) => {
       chai
-        .request(server)
+        .request(app)
         .get('/api/v1/genres')
         .end((err, res) => {
-          res.body.should.have.property('status').eql('200');
-          res.body.shoud.be.an('object');
-          res.body.should.have.property('data').eql('object');
+          res.body.should.have.property('status').eql(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('data').to.be.an('array');
         });
-        done();
+      done();
     });
   });
 });
