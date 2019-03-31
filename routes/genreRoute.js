80b47972e820291a5992131b87f6eb337691f5fc
an-apluss <anuoluwapoakinseye@gmail.com>
2019-03-31 06:14:48 +0100

    Add route to update a genre

--- routes/genreRoute.js
+++ routes/genreRoute.js
@@ -5,6 +5,7 @@ import GenreController from '../controllers/genreController';
 const router = express.Router();
 
 router.get('/:id', GenreController.getSingleGenre);
+router.put('/:id', GenreController.updateGenre);
 router.get('/', GenreController.getAllgenre);
 router.post('/', GenreController.postGenre);
 
