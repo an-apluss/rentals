77e695613c9f7b547ddde805438199d74e49d589
an-apluss <anuoluwapoakinseye@gmail.com>
2019-03-31 05:12:04 +0100

    Add route to fetch a single genre

--- routes/genreRoute.js
+++ routes/genreRoute.js
@@ -4,6 +4,7 @@ import GenreController from '../controllers/genreController';
 
 const router = express.Router();
 
+router.get('/:id', GenreController.getSingleGenre);
 router.get('/', GenreController.getAllgenre);
 router.post('/', GenreController.postGenre);
 
