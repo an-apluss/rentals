66aea6955d23850d51a63b72fe1a4c6ae351f8e9
an-apluss <anuoluwapoakinseye@gmail.com>
2019-04-13 09:26:51 +0100

    Include controller to insert a new customer

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,6 +6,18 @@ class CustomerController {
     if (!customer) return res.json({ status: 404, error: 'No customer to fetch' });
     return res.json({ status: 200, data: customer });
   }
+
+  static postCustomer(req, res) {
+    const lastCustomerId = dummydata.customers[dummydata.customers.length - 1].id;
+    const newCustomerId = lastCustomerId + 1;
+    const { firstname, lastname, phone } = req.body;
+    const newCustomer = {
+      id: newCustomerId, firstname, lastname, phone,
+    };
+    const insertNewCustomer = dummydata.customers.push(newCustomer);
+    if (!insertNewCustomer) return res.json({ status: 401, error: 'Customer cannot be added' });
+    return res.json({ status: 201, message: 'customer successfully added' });
+  }
 }
 
 export default CustomerController;
