dddaf1b362e4a0731878ea5208a378427b5b8839
an-apluss <anuoluwapoakinseye@gmail.com>
2019-04-13 07:55:41 +0100

    Add dummydata for customers

---
+++ datastorage/customer.js
@@ -0,0 +1,13 @@
+export default {
+  customers: [
+    {
+      id: 1, firstname: 'Anuoluwapo', lastname: 'Akinseye', phone: '08134326603',
+    },
+    {
+      id: 2, firstname: 'Bola', lastname: 'Aare', phone: '08024326603',
+    },
+    {
+      id: 3, firstname: 'Christy', lastname: 'Ambode', phone: '08024329903',
+    },
+  ],
+};
