2488c3fd427b15ad1c303a0bf27e5eaf0c18b765
an-apluss <anuoluwapoakinseye@gmail.com>
2019-04-15 10:01:39 +0100

    Revert "Include movieId to customer data"
    This reverts commit a797a047f976ee4fa2482f650548b125984bde56.

--- datastorage/customer.js
+++ datastorage/customer.js
@@ -1,13 +1,13 @@
 export default {
   customers: [
     {
-      id: 1, firstname: 'Anuoluwapo', lastname: 'Akinseye', phone: '08134326603', movieId: 7,
+      id: 1, firstname: 'Anuoluwapo', lastname: 'Akinseye', phone: '08134326603',
     },
     {
-      id: 2, firstname: 'Bola', lastname: 'Aare', phone: '08024326603', movieId: 2,
+      id: 2, firstname: 'Bola', lastname: 'Aare', phone: '08024326603',
     },
     {
-      id: 3, firstname: 'Christy', lastname: 'Ambode', phone: '08024329903', movieId: 5,
+      id: 3, firstname: 'Christy', lastname: 'Ambode', phone: '08024329903',
     },
   ],
 };
