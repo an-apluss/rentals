a73862dafbb78b2884c42a32a5bba7a91137a426
an-apluss <anuoluwapoakinseye@gmail.com>
2019-05-03 08:48:15 +0100

    Include user endpoints to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
@@ -6,6 +7,7 @@ import 'dotenv/config';
 import genreRoute from './routes/genreRoute';
 import customerRoute from './routes/customerRoute';
 import movieRoute from './routes/movieRoute';
+import userRoute from './routes/userRoute';
 
 
 const app = express();
@@ -13,16 +15,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api/v1/genres', genreRoute);
-app.use('/api/v1/customer', customerRoute);
-app.use('/api/v1/movie', movieRoute);
-
 app.get('/', (req, res) => {
   res.status(200).send({
     status: 200,
     message: 'Hello this is a Rental App',
   });
 });
+
+app.use('/api/v1/auth', userRoute);
+app.use('/api/v1/genres', genreRoute);
+app.use('/api/v1/customer', customerRoute);
+app.use('/api/v1/movie', movieRoute);
 
 const { PORT } = process.env;
 app.listen(PORT, () => console.log(`server is running on port ${PORT}...`));
