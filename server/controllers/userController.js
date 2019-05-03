782a7b351ded77c74702cdb13387ba43a3f044f8
an-apluss <anuoluwapoakinseye@gmail.com>
2019-05-03 08:52:05 +0100

    Include user controller to signin user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,10 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable linebreak-style */
+import bycrpt from 'bcrypt';
 import {
   User,
   validateSignup,
+  validateSignin,
   generateAuthToken,
 } from '../models/user';
 
@@ -17,6 +19,21 @@ class UserController {
     if (!newUser) return res.status(400).json({ status: 400, error: 'Incorrect User Type' });
     const token = generateAuthToken(newUser);
     return res.status(201).json({ status: 201, data: [{ token, user: newUser }], message: 'User Successfully Created' });
+  }
+
+  async postSignin(req, res) {
+    const user = new User();
+    const { error } = validateSignin(req.body);
+    if (error) return res.status(400).json({ status: 400, error: error.details[0].message });
+    const userExist = user.fetchAll().find(u => u.email === req.body.email);
+    if (!userExist) {
+      return res.status(400).json({ status: 400, error: 'Email or Password is Incorrect' });
+    }
+    const validPassword = await bycrpt.compare(req.body.password, userExist.password);
+    if (!validPassword) {
+      return res.status(400).json({ status: 400, error: 'Email or Password is Incorrect' });
+    }
+    return res.status(200).json({ status: 200, data: [{ token: generateAuthToken(userExist) }] });
   }
 }
 
