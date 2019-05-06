63d96c5528eed61dbef6b9840640e4a6bfd302dd
an-apluss <anuoluwapoakinseye@gmail.com>
2019-05-06 03:41:29 +0100

    Update user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,21 @@
 /* eslint-disable linebreak-style */
 
 import express from 'express';
+import Auth from '../middleware/Auth';
+import UserValidation from '../middleware/UserValidator';
 import UserController from '../controllers/userController';
 
+const { signupCheck, signinCheck } = UserValidation;
 const { postSignup, postSignin } = UserController;
+const {
+  getUser,
+  adminCheck,
+} = Auth;
 
 const router = express.Router();
 
-router.post('/signup', postSignup);
-router.post('/signin', postSignin);
+router.post('/signup', signupCheck, postSignup);
+router.post('/signin', signinCheck, postSignin);
+router.post('/createuser', getUser, adminCheck, signupCheck, postSignup);
 
 export default router;
